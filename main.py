5c7af0c11c16941478cc89bc1cb522f9c8bc88bf
marl-Am <marl-Am@users.noreply.github.com>
2025-06-29 18:57:26 -0400

    Can get top IPs, URLs, Status Code Distribution
    Used Chapters
    1.4(find smallest/largest),
    1.12(most frequent items),
    1.15 (grouping),
    3.15(datetime parsing)

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 # Entry point
 from logs.loader import load_logs
+from utils.analyzer import get_top_ips, get_top_urls, get_status_distribution
 
 if __name__ == "__main__":
     logs = load_logs("logs/sample.log")
-    # Print out 5 lines
-    for entry in logs[:5]:
-        print(entry)
+    # Testing logs/loader.py
+    # for entry in logs[:5]:
+    #     print(entry)
+
+    # Testing utils/analyzer.py
+    print("\n\nTop IPs:")
+    for ip, count in get_top_ips(logs):
+        print(f"{ip}: {count} requests")
+
+    print("\n\nTop URLs:")
+    for url, count in get_top_urls(logs):
+        print(f"{url}: {count} hits")
+
+    print("\n\nStatus Code Distribution:")
+    for status, count in get_status_distribution(logs).items():
+        print(f"{status}: {count}")
--- utils/analyzer.py
+++ utils/analyzer.py
@@ -1,1 +1,15 @@
 # Data aggregation logic
+from collections import Counter
+from typing import List, Dict
+
+def get_top_ips(logs: List[Dict], top_n: int = 5):
+    ip_counter = Counter(log['ip'] for log in logs)
+    return ip_counter.most_common(top_n)
+
+def get_top_urls(logs: List[Dict], top_n: int = 5):
+    url_counter = Counter(log['url'] for log in logs)
+    return url_counter.most_common(top_n)
+
+def get_status_distribution(logs: List[Dict]):
+    status_counter = Counter(log['status'] for log in logs)
+    return dict(status_counter)
